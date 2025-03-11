618179e449f7dbadcf0408a60976420b5897c2cd
Sdcllp <it.sdcllp@gmail.com>
2025-03-11 11:17:43 +0530

    Update Navbar.jsx

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,20 +1,15 @@
+import React from "react";
+
 const Navbar = ({ onLogout }) => {
   return (
     <div
       style={{
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-between",
+        justifyContent: "space-between", // Content ko left & right distribute karega
         padding: "12px 20px",
         backgroundColor: "#9fb6c3",
         color: "white",
-        width: "100vw", // Full width
-        position: "fixed", // Stick to the top
-        top: 0,
-        left: 0,
-        height: "60px", // Fixed height
-        zIndex: 1000, // Ensure it stays on top
-        boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)", // Add shadow
       }}
     >
       {/* Logo / Title */}
